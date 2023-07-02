5a125276c6069925243973ce37b382679236dc10
bekbolot aipidinov <aipidinovbekbolot135@gmail.com>
2023-07-02 16:31:38 +0600

    hw2

--- product/views.py
+++ product/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from .models import Category, Product, Review
 from .serializers import CategorySerializer, ProductSerializer, ReviewSerializer
